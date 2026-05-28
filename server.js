eada833c2302992668706062b2d19474841ef376
alessiodon10-hue <alessiodon10@gmail.com>
2026-05-28 13:46:48 +0200

    Update and rename Java to server.js

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// SERVE FILE STATICI
 app.use(express.static(path.join(__dirname, "public")));
 
-// HOMEPAGE
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
